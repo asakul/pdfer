12b1ff35b6ed3b4d8abb6b58a8c25a3b6c77f53c
Denis Tereshkin <denis@kasan.ws>
2015-06-03 16:25:44 +0600

    Arguments parsing

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 
 from feedparsers.livejournal import LivejournalParser
 from pdfsaver import PdfSaver
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Blog-to-pdf converter")
+    parser.add_argument('-o', '--output', action='store', dest='output', help='Output pdf filename', required=True)
+    parser.add_argument('-u', '--url', action='store', dest='url', help='URL of blog', required=True)
+
+    args = parser.parse_args()
+
     lj = LivejournalParser()
-    index_url = "http://anlazz.livejournal.com/"
-    saver = PdfSaver("test.pdf")
+    index_url = args.url
+    saver = PdfSaver(args.output)
     saver.save(lj, index_url)
 
 if __name__ == "__main__":
